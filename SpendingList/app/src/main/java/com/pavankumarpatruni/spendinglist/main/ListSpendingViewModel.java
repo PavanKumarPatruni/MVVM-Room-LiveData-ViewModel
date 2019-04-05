0b54a5fdea42467b6374bf6632022ad359400fe1
Pavan Kumar <pavanpathro@gmail.com>
2019-04-05 19:32:08 +0530

    Update ListSpendingViewModel.java

--- SpendingList/app/src/main/java/com/pavankumarpatruni/spendinglist/main/ListSpendingViewModel.java
+++ SpendingList/app/src/main/java/com/pavankumarpatruni/spendinglist/main/ListSpendingViewModel.java
@@ -8,44 +8,27 @@ import android.support.annotation.NonNull;
 
 import com.pavankumarpatruni.spendinglist.roomDB.AppDatabase;
 import com.pavankumarpatruni.spendinglist.roomDB.SpendingModel;
+import com.pavankumarpatruni.spendinglist.roomDB.SpendingsRepository;
 
 import java.util.List;
 
 public class ListSpendingViewModel extends AndroidViewModel {
 
-    private AppDatabase appDatabase;
-    private final LiveData<List<SpendingModel>> spendingModelList;
+    private SpendingsRepository spendingsRepository;
 
     public ListSpendingViewModel(@NonNull Application application) {
         super(application);
 
-        appDatabase = AppDatabase.getDatabase(this.getApplication());
+        spendingsRepository = new SpendingsRepository(application);
 
-        spendingModelList = appDatabase.getSpendingModel().getSpendingList();
     }
 
     public LiveData<List<SpendingModel>> getItemAndPersonList() {
-        return spendingModelList;
+        return spendingsRepository.getItemAndPersonList();
     }
 
     public void deleteItem(SpendingModel spendingModel) {
-        new deleteAsyncTask(appDatabase).execute(spendingModel);
-    }
-
-    private static class deleteAsyncTask extends AsyncTask<SpendingModel, Void, Void> {
-
-        private AppDatabase db;
-
-        deleteAsyncTask(AppDatabase appDatabase) {
-            db = appDatabase;
-        }
-
-        @Override
-        protected Void doInBackground(final SpendingModel... params) {
-            db.getSpendingModel().deleteSpending(params[0]);
-            return null;
-        }
-
+        spendingsRepository.deleteItem(spendingModel);
     }
 
 }
