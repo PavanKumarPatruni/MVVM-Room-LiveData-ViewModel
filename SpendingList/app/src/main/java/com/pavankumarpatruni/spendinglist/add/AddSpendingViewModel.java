f8fcbc294e0006d8b47b3503647d4394a49bc641
Pavan Kumar <pavanpathro@gmail.com>
2019-04-05 19:31:46 +0530

    Update AddSpendingViewModel.java

--- SpendingList/app/src/main/java/com/pavankumarpatruni/spendinglist/add/AddSpendingViewModel.java
+++ SpendingList/app/src/main/java/com/pavankumarpatruni/spendinglist/add/AddSpendingViewModel.java
@@ -1,34 +1,25 @@
-package com.pavankumarpatruni.spendinglist.main;
+package com.pavankumarpatruni.spendinglist.add;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
-import android.arch.lifecycle.LiveData;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
-import com.pavankumarpatruni.spendinglist.roomDB.AppDatabase;
 import com.pavankumarpatruni.spendinglist.roomDB.SpendingModel;
 import com.pavankumarpatruni.spendinglist.roomDB.SpendingsRepository;
 
-import java.util.List;
+public class AddSpendingViewModel extends AndroidViewModel {
 
-public class ListSpendingViewModel extends AndroidViewModel {
+    private final SpendingsRepository spendingsRepository;
 
-    private SpendingsRepository spendingsRepository;
-
-    public ListSpendingViewModel(@NonNull Application application) {
+    public AddSpendingViewModel(@NonNull Application application) {
         super(application);
 
         spendingsRepository = new SpendingsRepository(application);
 
     }
 
-    public LiveData<List<SpendingModel>> getItemAndPersonList() {
-        return spendingsRepository.getItemAndPersonList();
-    }
-
-    public void deleteItem(SpendingModel spendingModel) {
-        spendingsRepository.deleteItem(spendingModel);
+    public void addSpending(SpendingModel spendingModel) {
+        spendingsRepository.addSpending(spendingModel);
     }
 
 }
